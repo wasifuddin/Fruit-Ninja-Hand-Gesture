27ca4d35190e66fb6bdd2204d99f048073e13f6d
wasifuddin <79798771+wasifuddin@users.noreply.github.com>
2022-08-01 22:48:34 +0600

    Add files via upload

---
+++ fruit_ninja_ges.py
@@ -0,0 +1,35 @@
+# pip install opencv-contrib-python
+# pip install cvzone
+# pip install mouse
+# pip install numpy
+# pip install pyautogui
+
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import mouse
+import numpy as np
+import pyautogui
+
+cap = cv2.VideoCapture(1)
+cam_w, cam_h = 640, 480
+cap.set(3, cam_w)
+cap.set(4, cam_h)
+frameR = 100
+detector = HandDetector(detectionCon=0.65, maxHands=1)
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+    hands, img = detector.findHands(img)
+    cv2.rectangle(img, (frameR, frameR), (cam_w - frameR, cam_h - frameR), (255, 0, 255), 2)
+    if hands:
+        lmlist = hands[0]['lmList']
+        ind_x, ind_y = lmlist[8][0], lmlist[8][1]
+        cv2.circle(img, (ind_x, ind_y), 5, (0, 255, 255), 2)
+        conv_x = int(np.interp(ind_x, (frameR, cam_w - frameR), (0, 1920)))
+        conv_y = int(np.interp(ind_y, (frameR, cam_h - frameR), (0, 1080)))
+        mouse.move(conv_x, conv_y)
+        fingers = detector.fingersUp(hands[0])
+        if fingers[4] == 1:
+                pyautogui.mouseDown()
+    cv2.imshow("Camera Feed", img)
+    cv2.waitKey(1)
